ac118afb78ba6eb5cb6ef8aa6b1c57858284d1c1
Kay <uhbonaparte@gmail.com>
2025-04-04 13:49:32 +0000

    Create 4.cpp

---
+++ day1/4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+// type defs are used for creating aliases for data types
+using namespace std;
+
+typedef std::string ss; 
+
+int main() {
+
+ss x = "Hello";
+
+cout << x << endl;
+
+return 0;
+}
