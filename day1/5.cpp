539b1cee0dd20959eb27a6e74339748e0445f4a3
Kay <uhbonaparte@gmail.com>
2025-04-04 14:42:20 +0000

    Create 5.cpp

---
+++ day1/5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+// arithmetics or just math
+int meth = 20; //kg
+int cocaine = 23;
+int heroine = 21;
+int weed = 12;
+int ecstasy = 2;
+cout << meth + cocaine - heroine * weed / ecstasy;
+
+
+
+
+
+
+
+return 0;
+}
