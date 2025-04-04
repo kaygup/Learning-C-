5af5eda13c97162d6ec34b9fa3fda394a1a7bf3e
Kay <uhbonaparte@gmail.com>
2025-04-04 16:16:38 +0000

    Create 8.cpp

---
+++ day1/8.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+// hypothenuse calculator
+using namespace std;
+
+int main() {
+
+double a;
+double b;
+cout << "Welcome to the Hypothenuse calculator, please enter side a: ";
+cin >> a;
+cout << "\nplease enter side b: ";
+cin >> b;
+double result = (a*a) + (b*b);
+result = result;
+cout << "Your Hypothenuse is " + to_string(result) + "cm long" << endl;
+
+
+
+
+
+
+
+
+
+return 0;
+}
