9940101234680f196278bae7c43a886706c152c6
Kay <uhbonaparte@gmail.com>
2025-04-04 13:00:00 +0000

    Create 2.cpp

---
+++ day1/2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+// Calculating the circumference of a circle with a preset radius
+
+
+const double PI = 3.14159;
+
+double radius = 12.32;
+double circumference = 2 * PI * radius;
+
+cout << circumference << endl;
+
+
+return 0;
+}
