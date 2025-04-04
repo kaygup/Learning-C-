0a7b67f48dcdaf1c559342f68f1f8e4c291ee21f
Kay <uhbonaparte@gmail.com>
2025-04-04 15:51:42 +0000

    Create 6.cpp

---
+++ day1/6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// user input
+using namespace std;
+
+int main() {
+
+string kg;
+cout << "How much meth did you cook (in kg)?: ";
+cin >> kg;
+
+cout << "You cooked " + kg + "kg of meth" << endl;
+return 0;
+}
